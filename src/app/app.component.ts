072036b246b1cf3811f33c0079bbf0ec335ee02c
M Noman Khalid <bhattinoman581@gmail.com>
2024-07-04 12:25:15 -0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,8 @@ import { RouterLink, RouterOutlet, RouterLinkActive } from '@angular/router';
 import { SignInModalComponent } from './sign-in-modal/sign-in-modal.component';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { Observable, of } from 'rxjs';
+import { CourseService } from './core/course.service';
 
 @Component({
   selector: 'app-root',
@@ -17,9 +19,11 @@ export class AppComponent implements OnInit {
   date: number = 0;
   menuHidden = true;
   isSignInModalVisible = false;
-  searchQuery: string = ''; 
+  searchQuery: string = '';
+  searchSuggestions$: Observable<string[]> | undefined;
+  selectedSuggestion: string | undefined;
 
-  constructor(private router: Router) {} 
+  constructor(private router: Router, private courseService: CourseService) {}
 
   ngOnInit(): void {
     this.date = new Date().getFullYear();
@@ -37,13 +41,37 @@ export class AppComponent implements OnInit {
     this.isSignInModalVisible = false;
   }
 
-  searchCourses(): void { 
+  searchCourses(): void {
+    if (this.selectedSuggestion) {
+      this.searchQuery = this.selectedSuggestion;
+    }
     this.router.navigate(['/courses'], { queryParams: { search: this.searchQuery } });
   }
 
-  handleKeypress(event: KeyboardEvent): void {
-    if (event.key === 'Enter') {
-      this.searchCourses();
+  // handleKeypress(event: KeyboardEvent): void {
+  //   if (event.key === 'Enter') {
+  //     this.searchCourses();
+  //   }  
+  // }
+
+  onInputChange(): void {
+    if (this.searchQuery.length >= 1) {
+      const courses = this.courseService.getCourses();
+      const filteredSuggestions = courses
+        .map(course => course.title)
+        .filter(title => title.toLowerCase().includes(this.searchQuery.toLowerCase()))
+        .slice(0, 14);
+  
+      this.searchSuggestions$ = of(filteredSuggestions);
+    } else {
+      this.searchSuggestions$ = of([]);
     }
+  }
+
+  selectSuggestion(suggestion: string): void {
+    this.selectedSuggestion = suggestion;
+    this.searchQuery = suggestion;
+    this.searchSuggestions$ = undefined;
+    this.router.navigate(['/courses'], { queryParams: { search: this.searchQuery } });
   }
 }
